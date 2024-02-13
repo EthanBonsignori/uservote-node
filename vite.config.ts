39130516cdd1d2b185979d0b2af4515d4a0663ab
Ethan Bonsignori <ebonsignori@gmail.com>
2024-02-13 09:59:14 -0800

    add vite config type completion

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
     port: 3000,
+    strictPort: true,
   },
 });
