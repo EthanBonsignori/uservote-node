394bf29c4dda2e3bf08d49cb86fdf19b45db882c
Ethan Bonsignori <ebonsignori@gmail.com>
2024-02-15 14:31:26 -0800

    create Navbar component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,10 @@
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 import { FC, PropsWithChildren } from "react";
-
-import styled from "@emotion/styled";
-
-const AppBarStyled = styled(AppBar)`
-  margin-bottom: 20px;
-`;
+import Navbar from "./Navbar";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div>
-      <AppBarStyled color="primary" position="sticky">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            UserVote
-          </Typography>
-          {/* TODO: login */}
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBarStyled>
+      <Navbar />
       {children}
     </div>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,92 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { FC, useState } from "react";
+
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Try it out"];
+
+const Navbar: FC = () => {
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        UserVote
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <AppBar color="primary" position="sticky" sx={{ mb: "20px" }}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            UserVote
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button key={item} color="inherit">
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav aria-hidden={!drawerOpen}>
+        <Drawer
+          variant="temporary"
+          open={drawerOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
