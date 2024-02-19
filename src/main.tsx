35ce1d4fd5ea6b6ce976887e4f133309841dc21f
Ethan Bonsignori <ebonsignori@gmail.com>
2024-02-18 19:16:29 -0800

    add theme, cssbaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
@@ -5,6 +7,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/Root";
+import theme from "./theme";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +16,10 @@ const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+export default createTheme({
+  palette: {
+    background: {
+      default: "#C8EFF9",
+    },
+    primary: {
+      main: "#F27D42",
+      dark: "#DF622C",
+      light: "#F07C41",
+      contrastText: "#FFFFFF",
+    },
+  },
+});
